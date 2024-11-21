9b5f1af69c00dd5a6c97b41b7fb523414a545e6b
akloppenburg2 <a.c.kloppenburg@wustl.edu>
2024-11-21 15:50:30 -0600

    Completed studio 14

---
+++ studio14/Studio13.md
@@ -0,0 +1,32 @@
+# Studio 13
+
+1. Student Names
+    1. Sam Yoo
+    2. Alex Kloppenburg
+    3. Ben Kim
+
+2. Print Integers
+    1. Code
+        1. ![alt text](images/2-1.png)
+    2. Output
+        1. ![alt text](images/2-2.png)
+
+3. Print Odds
+    1. Output
+        1. ![alt text](images/3-1.png)
+
+4. Collect and Map
+    1. Code
+        1. ![alt text](images/4-1.png)
+
+5. Primes
+    1. Output
+        1. ![alt text](images/5-1.png)
+
+6. Using for_each
+    1. Code
+        1. ![alt text](images/6-1.png)
+
+7. Optimized
+    1. Code
+        1. ![alt text](images/7-1.png)
--- studio14/src/main.rs
+++ studio14/src/main.rs
@@ -1,3 +1,45 @@
 fn main() {
-    println!("Hello, world!");
+    // Declare string
+    let mut num_string: String;
+
+    // Declare and initialize vector
+    let mut primes: Vec<bool> = (0..=99).map(|x| (x % 2 == 1 && x > 2) || x == 2).collect(); 
+
+    // Construct table
+    for num in 0..=99
+    {
+        // If we hit a value that's true, mark all of its multiples false
+        if primes[num]
+        {
+            ((num * num)..=99).step_by(2 * num).for_each(|x| primes[x] = false)
+        }
+
+        // If we hit a multiple of 10, insert a newline, otherwise concatenate normally
+        // If the value is false in the vector, simple print spaces
+        if primes[num]
+        {
+            if num % 10 == 9
+            {
+                num_string = format!("{:>2}\n", num);
+            }
+            else
+            {
+                num_string = format!("{:>2} ", num);
+            }
+        }
+        else
+        {
+            if num % 10 == 9
+            {
+                num_string = format!("  \n");
+            }
+            else
+            {
+                num_string = format!("   ");
+            }
+        }
+
+        // Print value
+        print!("{}", num_string);
+    }
 }

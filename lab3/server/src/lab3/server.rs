ceffb4c09d4b0a7a71011df6972e1971f499360d
akloppenburg2 <a.c.kloppenburg@wustl.edu>
2024-12-03 21:26:41 -0600

    Completed steps 9 and 11

--- lab3/server/src/lab3/server.rs
+++ lab3/server/src/lab3/server.rs
@@ -1,9 +1,10 @@
 // server.rs
 // Benjamin Kim, Alex Kloppenburg, Sam Yoo
 // Lab3 Server
-use std::net::TcpListener;
-use std::sync::atomic::AtomicBool;
+use std::net::{TcpListener, TcpStream};
+use std::sync::atomic::{Ordering, AtomicBool};
 use std::io::{stdout, stderr, Write};
+use std::thread;
 
 // Static CANCEL_FLAG for graceful shutdown
 pub static CANCEL_FLAG: AtomicBool = AtomicBool::new(false);
@@ -40,5 +41,44 @@ impl Server {
                 writeln!(stderr().lock(), "Error: Failed to bind server to address {}: {}", addr, e).unwrap();
             }
         }
+    }
+
+    // Run server 
+    pub fn run(&self) {
+        // Verify we have a listener first
+        match &self.listener {
+            Some(listener) => {
+                while !CANCEL_FLAG.load(Ordering::SeqCst) {
+                    // Accept a connection
+                    match listener.accept() {
+                        Ok((socket, addr)) => {
+                            // Check cancel flag again and immediately exit if true
+                            if CANCEL_FLAG.load(Ordering::SeqCst) {
+                                return; // Exit if cancel flag is set
+                            }
+                            writeln!(stdout().lock(), "Accepted connection from {}", addr).unwrap();
+                            // Spawn a thread to handle the connection
+                            thread::spawn(move || {
+                                Self::handle_connection(socket);
+                            });
+                        }
+                        Err(e) => {
+                            writeln!(stderr().lock(), "Error: Failed to accept connection: {}", e).unwrap();
+                            // Check cancel flag again and immediately exit if true
+                            if CANCEL_FLAG.load(Ordering::SeqCst) {
+                                return; // Exit if cancel flag is set
+                            }
+                        }
+                    }
+                }
+            },
+            None => writeln!(stderr().lock(), "Error: Server is not open. Call open() before run().").unwrap(),
+        }
+        writeln!(stdout().lock(), "Server shutting down...").unwrap();
+    }
+
+    pub fn handle_connection(socket: TcpStream)
+    {
+
     }
 }
--- lab3/server/src/main.rs
+++ lab3/server/src/main.rs
@@ -1,9 +1,61 @@
 // server.rs
 // Benjamin Kim, Alex Kloppenburg, Sam Yoo
 // Lab3 Server Implementation
+use std::io::{stdout, stderr, Write};
+use std::env;
+
+use crate::lab3::server::Server;
+use crate::lab3::return_wrapper::ReturnWrapper;
 
 pub mod lab3;
 
-fn main() {
-    println!("Hello, world!");
+// Command line constants
+const NUM_ARGS: usize = 2;          // Number of command line arguments
+const PROGRAM_NAME: usize = 0;      // Position of the name of the program in the vector produced by env::args.collect()
+const ADDRESS_INDEX: usize = 1;      // Position of the network address in the vector produced by env::args.collect()
+
+// Error constants
+const CMD_LINE_ERR: u8 = 1;         // Error for command line argument issues
+
+fn main() -> ReturnWrapper {
+    writeln!(stdout().lock(), "Starting the Lab3 Server...").unwrap();
+
+    let mut address = String::new();
+
+    // Parse arguments from the command line
+    if let Err(e) = parse_args(&mut address) {
+        writeln!(stderr().lock(), "Error parsing arguments!").unwrap();
+        return ReturnWrapper::new(Err(e));  // Return error for bad command line arguments
+    }
+
+    // If successful, create server, open, and run
+    let mut server = Server::new();
+
+    // Open the server on the specified address
+    server.open(&address);
+    
+    // Run the server
+    server.run();
+
+    ReturnWrapper::new(Ok(()))
+}
+
+fn usage(name: &str) {
+    println!("usage: {} <network_address>", name);
+}
+
+fn parse_args(address: &mut String) -> Result<(), u8> {
+    
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != NUM_ARGS {
+        usage(&args[PROGRAM_NAME]);
+        return Err(CMD_LINE_ERR);
+    }
+    else
+    {
+        *address = args[ADDRESS_INDEX].clone();
+    }
+
+    Ok(())
 }
